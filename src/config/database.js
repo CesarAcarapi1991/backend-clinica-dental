7e0980105e83f3d66791af08afa7312165528a1d
CesarAcarapi <cesar21908@gmail.com>
2026-03-12 21:27:56 -0400

    Habilitacion de configuracion SSL

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,19 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+//   pool
+// };
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -7,6 +23,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = {
